e0d66d45d5907c0e68de3b838c993f57a92a300a
shahzodbekT <mirahmedovshahzodbek@gmail.com>
2023-11-20 22:01:49 +0500

    upd

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ jokeField = document.getElementById('joke')
 
 // Functions
 async function getChuckNorrisJoke() {
-  let url = `https://api.chucknorris.io/jokes/random    `
-  let response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    }
-  })
-  response = await response.json()
-  jokeField.textContent = JSON.stringify(response.value)
-  console.log(JSON.stringify(response.value))
+  try {
+    let url = `https://api.chucknorris.io/jokes/random    `
+    let response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }
+    })
+    response = await response.json()
+    jokeField.textContent = JSON.stringify(response.value)
+    console.log(JSON.stringify(response.value))
+  } catch (error) {
+    console.log('Error: ' + error)
+  }
 }
