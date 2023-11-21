a6085aa7a5c78ac5f5fd09ce0f9629ba3347b0aa
shahzodbekT <mirahmedovshahzodbek@gmail.com>
2023-11-21 16:56:52 +0500

    upd-less-code

--- main.js
+++ main.js
@@ -7,10 +7,6 @@ async function getChuckNorrisJoke() {
     let url = `https://api.chucknorris.io/jokes/random    `
     let response = await fetch(url, {
       method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      }
     })
     response = await response.json()
     jokeField.textContent = JSON.stringify(response.value)
